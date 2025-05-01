85bafc37fe786937221055a90d60c680ab0cd193
Jesus Zuñiga Trejo <zutjmx@gmail.com>
2025-05-01 11:26:49 -0600

    Refine weather condition descriptions in Spanish for improved accuracy

--- script.js
+++ script.js
@@ -150,34 +150,45 @@ const weatherDescriptions = (codigo) => {
         case 0:
             return "Despejado";
         case 1:
-            return "Parcialmente nublado";
         case 2:
-            return "Nublado";
         case 3:
+            return "Mayormente soleado";
+        case 45:
+        case 48:
+            return "Niebla y escarcha";
+        case 51:
+        case 53:
+        case 55:
             return "Lluvia ligera";
-        case 4:
+        case 56:
+        case 57:
+            return "Lluvia helada ligera";
+        case 61:
+        case 63:
+        case 65:
             return "Lluvia moderada";
-        case 5:
-            return "Tormenta eléctrica";
+        case 66:
+        case 67:
+            return "Lluvia helada";
+        case 71:
+        case 73:
+        case 75:
+            return "Nieve ligera";
+        case 77:
+            return "Granos de nieve";
+        case 80:
+        case 81:
+        case 82:
+            return "Chubascos de lluvia";
+        case 85:
+        case 86:
+            return "Chubascos de nieve";
+        case 95:
+            return "Tormenta ligera o moderada";
+        case 96:
+        case 99:
+            return "Tormenta con granizo ligero o fuerte";
         default:
             return "Desconocido";
     }
-
-    //TODO: Agregar más condiciones para los códigos de clima
-    /** Code	Description
-        0	Clear sky
-        1, 2, 3	Mainly clear, partly cloudy, and overcast
-        45, 48	Fog and depositing rime fog
-        51, 53, 55	Drizzle: Light, moderate, and dense intensity
-        56, 57	Freezing Drizzle: Light and dense intensity
-        61, 63, 65	Rain: Slight, moderate and heavy intensity
-        66, 67	Freezing Rain: Light and heavy intensity
-        71, 73, 75	Snow fall: Slight, moderate, and heavy intensity
-        77	Snow grains
-        80, 81, 82	Rain showers: Slight, moderate, and violent
-        85, 86	Snow showers slight and heavy
-        95 *	Thunderstorm: Slight or moderate
-        96, 99 *	Thunderstorm with slight and heavy hail 
-    */
-
 }
